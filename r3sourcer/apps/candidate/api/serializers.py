a02ed633f9428f3e6c5f9475e0c959cb0be90a74
stassyn <stasinyavskiy@gmail.com>
2017-11-08 11:48:02 +0200

    RV-235 Refactored candidate serializers

--- r3sourcer/apps/candidate/api/serializers.py
+++ r3sourcer/apps/candidate/api/serializers.py
@@ -1,13 +1,13 @@
-from django.utils.translation import ugettext_lazy as _
 from rest_framework import exceptions, serializers
-from r3sourcer.apps.hr.api.serializers import timesheet as timesheet_serializers
-from r3sourcer.apps.activity.endpoints.serializers import ActivitySerializer
-from r3sourcer.apps.candidate import models
+from django.utils.translation import ugettext_lazy as _
+
 from r3sourcer.apps.hr import models as hr_models
+from r3sourcer.apps.core import models as core_models
+from r3sourcer.apps.skills import models as skill_models
+from r3sourcer.apps.candidate import models as candidate_models
 from r3sourcer.apps.core.api import serializers as core_serializers
-from r3sourcer.apps.core.models import Contact, Tag
-from r3sourcer.apps.skills.models import Skill
-from .. import models
+from r3sourcer.apps.hr.api.serializers import timesheet as timesheet_serializers
+from r3sourcer.apps.activity.endpoints import serializers as activity_serializers
 
 
 class FavouriteListSerializer(core_serializers.ApiBaseModelSerializer):
@@ -35,27 +35,24 @@ class CarrierListSerializer(core_serializers.ApiBaseModelSerializer):
 
 
 class SkillRelSerializer(core_serializers.ApiBaseModelSerializer):
-
     class Meta:
-        model = models.SkillRel
+        model = candidate_models.SkillRel
         fields = '__all__'
 
 
 class TagRelSerializer(core_serializers.ApiBaseModelSerializer):
-
     class Meta:
-        model = models.TagRel
+        model = candidate_models.TagRel
         fields = '__all__'
 
 
-class CandidateContactSerializer(core_serializers.ApiRealtedFieldManyMixin,
-                                 core_serializers.ApiBaseModelSerializer):
+class CandidateContactSerializer(core_serializers.ApiRealtedFieldManyMixin, core_serializers.ApiBaseModelSerializer):
     blacklist = BlackListSerializer(many=True)
     candidate_skills = SkillRelSerializer(many=True)
     tag_rels = TagRelSerializer(many=True)
     notes = serializers.NoteSerializer(many=True)
     active_states = serializers.WorkflowObjectSerializer(many=True)
-    activities = ActivitySerializer(many=True)
+    activities = activity_serializers.ActivitySerializer(many=True)
     favourites = FavouriteListSerializer(many=True)
     vacancy_offers = VacancyOfferSerializer(many=True)
     carrier_lists = CarrierListSerializer(many=True)
@@ -77,12 +74,12 @@ class CandidateContactSerializer(core_serializers.ApiRealtedFieldManyMixin,
 
     def create(self, validated_data):
         contact = validated_data.get('contact', None)
-        if not isinstance(contact, Contact):
+        if not isinstance(contact, core_models.Contact):
             raise exceptions.ValidationError(
                 _('Contact is required')
             )
 
-        if models.CandidateContact.objects.filter(contact=contact).exists():
+        if candidate_models.CandidateContact.objects.filter(contact=contact).exists():
             raise exceptions.ValidationError(
                 _('Candidate Contact with this Contact already exists.')
             )
@@ -91,7 +88,7 @@ class CandidateContactSerializer(core_serializers.ApiRealtedFieldManyMixin,
         return instance
 
     class Meta:
-        model = models.CandidateContact
+        model = candidate_models.CandidateContact
         fields = ('__all__', {
             'blacklists': ('__all__',),
             'favourites': ('__all__',),
@@ -167,12 +164,12 @@ class CandidateContactRegisterSerializer(core_serializers.ContactRegisterSeriali
     is_subcontractor = serializers.BooleanField(required=False)
     tags = serializers.PrimaryKeyRelatedField(
         required=True,
-        queryset=Tag.objects,
+        queryset=core_models.Tag.objects,
         many=True
     )
     skills = serializers.PrimaryKeyRelatedField(
         required=True,
-        queryset=Skill.objects,
+        queryset=skill_models.Skill.objects,
         many=True
     )
 
@@ -191,13 +188,13 @@ class CandidateContactRegisterSerializer(core_serializers.ContactRegisterSeriali
         candidate_contact = CandidateContactSerializer().create(candidate)
 
         for tag in tags:
-            models.TagRel.objects.create(
+            candidate_models.TagRel.objects.create(
                 candidate_contact=candidate_contact,
                 tag=tag,
             )
 
         for skill in skills:
-            models.SkillRel.objects.create(
+            candidate_models.SkillRel.objects.create(
                 candidate_contact=candidate_contact,
                 skill=skill,
             )
@@ -209,15 +206,13 @@ class CandidateContactRegisterSerializer(core_serializers.ContactRegisterSeriali
             'title', 'first_name', 'birthday', 'email', 'phone_mobile', 'tags',
             'last_name', 'picture', 'agree', 'skills', 'is_subcontractor',
             {
-                'address': ('country', 'state', 'city', 'street_address',
-                            'postal_code'),
+                'address': ('country', 'state', 'city', 'street_address', 'postal_code'),
                 'candidate': ('tax_file_number', )
             },
         )
 
 
 class SubcontractorSerializer(core_serializers.ApiBaseModelSerializer):
-
     class Meta:
         fields = '__all__'
-        model = models.Subcontractor
+        model = candidate_models.Subcontractor
