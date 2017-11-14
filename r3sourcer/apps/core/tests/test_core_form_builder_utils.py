97f8729dc3ba6dfcd25240abe3bd226b54cdf365
Serafim Suhenky <serafim@suhenky.com>
2017-11-14 16:48:04 +0300

    RV-125 * updated helpers * fixed tests

--- r3sourcer/apps/core/tests/test_core_form_builder_utils.py
+++ r3sourcer/apps/core/tests/test_core_form_builder_utils.py
@@ -1,10 +1,6 @@
 import pytest
-from django import forms
 
 from django.contrib.contenttypes.models import ContentType
-from django.core.exceptions import ValidationError
-from django.core.files.base import ContentFile
-from django.db import IntegrityError
 
 from r3sourcer.apps.core import models
 from r3sourcer.apps.core.utils import form_builder
@@ -12,37 +8,6 @@ from r3sourcer.apps.core.utils import form_builder
 
 @pytest.mark.django_db
 class TestModelStorageHelperCls:
-
-    @pytest.fixture
-    def form_builder(self):
-        return models.FormBuilder.objects.create(
-            content_type=ContentType.objects.get_for_model(models.Contact),
-
-        )
-
-    @pytest.fixture
-    def form(self, company, form_builder):
-        return models.Form.objects.create(
-            title='test form1',
-            builder=form_builder,
-            is_active=True,
-            company=company
-        )
-
-    @pytest.fixture
-    def form_field_group(self, form):
-        return models.FormFieldGroup.objects.create(
-            form=form,
-            name='General'
-        )
-
-    @pytest.fixture
-    def form_fields(self, form_field_group):
-        return [
-            models.ModelFormField.objects.create(group=form_field_group, name='phone_mobile', required=True),
-            models.ModelFormField.objects.create(group=form_field_group, name='email'),
-            models.ModelFormField.objects.create(group=form_field_group, name='first_name', required=True)
-        ]
 
     def test_simple_field(self):
         name, value = 'test_name', 'test value'
@@ -63,15 +28,13 @@ class TestModelStorageHelperCls:
         value = 'test value'
         related_field = form_builder.RelatedFieldHelper(models.CompanyContact, 'contact__first_name', value)
 
-        assert not related_field.simple_fields
-        assert not related_field.related_fields
-
-        related_field.process_field(value)
-
         assert related_field.value is None
         assert related_field.name == 'contact'
         assert 'first_name' in related_field.simple_fields
-        assert related_field.simple_fields['first_name'] == value
+        assert related_field.simple_fields['first_name'].value == value
+
+        with pytest.raises(AssertionError):
+            related_field.process_field(value)
 
     def test_storage_helper(self):
         storage_helper = form_builder.StorageHelper(models.CompanyContact, {
@@ -81,12 +44,11 @@ class TestModelStorageHelperCls:
             'contact__email': 'test@example.com',
             'contact__phone_mobile': '+79998887766'
         })
-
         storage_helper.process_fields()
 
         assert set(storage_helper.fields.keys()).issuperset({'contact', 'job_title'})
-        assert set(storage_helper.fields['contact'].simple_fields.keys).issuperset({'first_name', 'last_name',
-                                                                                    'phone_mobile', 'email'})
+        assert set(storage_helper.fields['contact'].simple_fields.keys()).issuperset({'first_name', 'last_name',
+                                                                                      'phone_mobile', 'email'})
 
         instance = storage_helper.create_instance()
         assert isinstance(instance, models.CompanyContact)
--- r3sourcer/apps/core/utils/form_builder.py
+++ r3sourcer/apps/core/utils/form_builder.py
@@ -4,19 +4,30 @@ from django.db import models
 from django.db import transaction
 
 
+class AlreadyProcessed(Exception):
+    pass
+
+
 class StorageHelper(object):
 
     __slots__ = ['model', 'fields', 'separator', '_fields']
 
-    def __init__(self, model, fields: dict, separator: str ='__'):
+    LOOKUP_SEPARATOR = '__'
+
+    def __init__(self, model, fields: dict):
+        assert issubclass(model, models.Model), "`model` should be models.Model subclass"
+
         self.model = model
-        self.separator = separator
         self._fields = fields
         self.fields = {}
 
     def process_fields(self):
+        """
+        
+        :return: 
+        """
         for field_name, field_value in self._fields.items():
-            if '__' in field_name:
+            if self.LOOKUP_SEPARATOR in field_name:
                 field = RelatedFieldHelper(self.model, field_name, field_value)
                 if field.name in self.fields:
                     old_field = self.fields.get(field.name)
@@ -28,12 +39,34 @@ class StorageHelper(object):
 
     @transaction.atomic
     def create_instance(self):
+        """
+        Create instance using fields (RelatedFields and SimpleFields)
+
+        :return: self.model instance.
+        """
         object_params = {}
         for name, field in self.fields.items():
             if isinstance(field, RelatedFieldHelper):
                 field.save_related()
             object_params.setdefault(name, field.value)
         return self.model.objects.create(**object_params)
+
+    @classmethod
+    def separate_lookup_name(cls, field_name: str) -> tuple():
+
+        """
+        Separate lookup names.
+        
+        >> StorageHelper.separate_lookup_name('user__name')
+        << ('user', 'name')
+        
+        >> StorageHelper.separate_lookup_name('name')
+        << ('name', '')
+
+        :return: tuple of str -> (first field name, lookup name)
+        """
+        field_name, _, lookup_name = field_name.partition(cls.LOOKUP_SEPARATOR)
+        return field_name, lookup_name
 
 
 class SimpleFieldHelper(object):
@@ -43,35 +76,42 @@ class SimpleFieldHelper(object):
     ]
 
     def __init__(self, name, value):
-        assert re.match('$\w+[\w\d_]*^', name), "Incorrect field name"
+        assert re.match('^[a-z]+[\w_]*$', name, re.IGNORECASE), "Incorrect field name"
 
         self.name = name
         self.value = value
 
 
 class RelatedFieldHelper(object):
+    """
+    Helper class for ForeignObject fields, would be used for storing and manipulation related object fields.
+    """
 
-    __slots__ = ['model', 'name', 'lookup_name', 'value', 'simple_fields', 'related_fields']
+    __slots__ = ['model', 'name', 'lookup_name', 'value', 'simple_fields', 'related_fields', '_done']
 
     def __init__(self, model, name, value):
 
-        assert re.match('$\w+[\w\d_]*^', name), "Incorrect field name"
+        assert re.match('^[a-z]+[\w_]*$', name, re.IGNORECASE), "Incorrect field name"
 
-        self.name, _, self.lookup_name = name.partition('__')
+        self.name, self.lookup_name = StorageHelper.separate_lookup_name(name)
         self.model = model._meta.get_field(self.name).related_model
 
         self.value = None
+        self._done = False
         self.simple_fields = {}
         self.related_fields = {}
 
         self.process_field(value)
 
     def process_field(self, value):
-        if '__' in self.lookup_name:
+        assert not self._done, "Field already processed"
+
+        if StorageHelper.LOOKUP_SEPARATOR in self.lookup_name:
             related_field = RelatedFieldHelper(self.model, self.lookup_name, value)
             self.related_fields.setdefault(related_field.name, related_field)
         else:
             self.simple_fields.setdefault(self.lookup_name, SimpleFieldHelper(self.lookup_name, value))
+        self._done = True
 
     @classmethod
     def merge(cls, field1, field2):
