c69e2efe067a89862deed5511d3e3b544772438b
Konstantin Surikhin <kostyasurihin@gmail.com>
2018-02-06 15:01:13 +0200

    RV-149 added relation to syncobject

--- r3sourcer/apps/myob/helpers.py
+++ r3sourcer/apps/myob/helpers.py
@@ -2,12 +2,12 @@ from . api import MYOBClient
 from . models import MYOBCompanyFileToken
 
 
-def get_myob_client(cf_id=None, company=None):
+def get_myob_client(cf_id=None, company=None, date=None):
     try:
-        if company:
-            cft = company.myobcompanyfiletoken
-        elif cf_id:
+        if cf_id:
             cft = MYOBCompanyFileToken.objects.get(company_file__cf_id=cf_id)
+        elif company:
+            cft = company.company_file_tokens.enabled(date).first()
         else:
             cft = MYOBCompanyFileToken.objects.first()
 
