0fe6c213461691142a3821c61162753298169659
Konstantin Surikhin <kostyasurihin@gmail.com>
2018-02-16 13:05:09 +0200

    RV-235 fixed system company rename

--- r3sourcer/apps/core/models/core.py
+++ r3sourcer/apps/core/models/core.py
@@ -894,6 +894,12 @@ class Company(
         default='4-1000',
     )
 
+    is_system = models.BooleanField(
+        default=False,
+        editable=False,
+        verbose_name=_('System Company')
+    )
+
     groups = models.ManyToManyField(Group, related_name='companies')
 
     class Meta:
