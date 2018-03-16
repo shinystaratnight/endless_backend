916577821f35654e87f71f70564f90dc39be39b0
Konstantin Surikhin <kostyasurihin@gmail.com>
2018-03-16 16:28:40 +0200

    RV-281 added company sync to myob

--- r3sourcer/apps/core/models/core.py
+++ r3sourcer/apps/core/models/core.py
@@ -743,9 +743,11 @@ class BankAccount(UUIDModel):
 
 
 class Company(
-        CategoryFolderMixin,
-        UUIDModel,
-        MasterCompanyLookupMixin):
+    CategoryFolderMixin,
+    MYOBMixin,
+    UUIDModel,
+    MasterCompanyLookupMixin
+):
 
     name = models.CharField(max_length=127, verbose_name=_("Company Name"), unique=True)
 
@@ -1017,6 +1019,10 @@ class Company(
             qs = qs.filter(price_list_rates__skill=position)
 
         return qs
+
+    def get_portfolio_manager(self):
+        company_rel = self.regular_companies.all().first()
+        return company_rel and company_rel.primary_contact
 
     @property
     def invoice_rule(self):
