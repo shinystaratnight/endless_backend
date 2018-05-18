def0a3c855fb5632fbd9feef031ce11bbd62d8b8
kos_sur <kostyasurihin@gmail.com>
2018-05-18 08:57:01 +0300

    RV-285 fixed company page

--- r3sourcer/apps/core/migrations/0031_removed_company_contact_voip.py
+++ r3sourcer/apps/core/migrations/0031_removed_company_contact_voip.py
@@ -8,7 +8,7 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('core', '0027_company_stripe_customer'),
+        ('core', '0030_remove_labdline_and_fax_from_address'),
     ]
 
     operations = [
