51269df07596cb8c588b2cc7dd79607109cf5daf
kos_sur <kostyasurihin@gmail.com>
2018-06-06 16:18:20 +0300

    RV-285 fixed company address validation

--- r3sourcer/apps/core/endpoints.py
+++ r3sourcer/apps/core/endpoints.py
@@ -365,12 +365,23 @@ class CompanyAddressEndpoint(ApiEndpoint):
     )
 
     fieldsets = (
-        'name', 'company', 'address',
+        'name', 'company',
         {
             'type': constants.FIELD_ADDRESS,
             'field': 'address',
         },
-        'hq', 'phone_landline', 'phone_fax', 'primary_contact', 'active'
+        'hq', 'phone_landline', 'phone_fax',
+        {
+            'type': constants.FIELD_RELATED,
+            'field': 'primary_contact',
+            'prefilled': {
+                'company': '{company.id}',
+            },
+            'query': {
+                'company': '{company.id}',
+            },
+        },
+        'active'
     )
 
     # FIXME: add for remaining columns and change to real labels and endpoints
