ccf46d84e19af99d05c46a0ea76bd86459c4bde7
kos_sur <kostyasurihin@gmail.com>
2018-06-20 11:43:34 +0300

    RV-666 fixed dashboard module django admin model

--- r3sourcer/apps/core/admin/dashboard.py
+++ r3sourcer/apps/core/admin/dashboard.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.contrib.contenttypes.models import ContentType
 
 from .. import forms
 from .. import models
@@ -17,7 +18,7 @@ class DashboardModuleAdmin(GuardianModelAdmin):
             excluded_ids.remove(obj.content_type_id)
 
         form = super(DashboardModuleAdmin, self).get_form(request, obj=obj, **kwargs)
-        form.base_fields['content_type'].queryset = form.base_fields['content_type'].queryset.exclude(
+        form.base_fields['content_type'].queryset = ContentType.objects.exclude(
             id__in=excluded_ids
         )
         return form
