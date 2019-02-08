a2ef92699820bb7dfd65967f4bd51b5587be96b1
denys-demchenko <denys.demchenko@sigma.software>
2019-02-08 13:51:56 +0200

    RV-618 added sub admin, fixed bug with subscriptions

--- r3sourcer/apps/billing/admin.py
+++ r3sourcer/apps/billing/admin.py
@@ -4,9 +4,18 @@ from . import models
 
 
 class SMSBalanceAdmin(admin.ModelAdmin):
+    list_display = ('company', 'balance',)
+    search_fields = ('company__name',)
 
-    list_display = ('company', 'balance', )
-    search_fields = ('company__name', )
+
+class TSubscriptionAdmin(admin.ModelAdmin):
+    list_display = ('__str__', 'status')
+    list_filter = ('company', 'type')
+    readonly_fields = ('company', 'last_time_billed', 'current_period_start',
+                       'current_period_end', 'worker_count', 'price', 'type',
+                       'created')
 
 
 admin.site.register(models.SMSBalance, SMSBalanceAdmin)
+admin.site.register(models.Subscription, TSubscriptionAdmin)
+admin.site.register(models.Payment)
