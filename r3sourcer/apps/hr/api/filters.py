3247a0cb27d3a57d08906f4cfdb20b2b0c7b9421
denys-demchenko <dendemchenko57@gmail.com>
2019-03-27 12:48:53 +0200

    RV-789 changed filter in joboffer

--- r3sourcer/apps/hr/api/filters.py
+++ r3sourcer/apps/hr/api/filters.py
@@ -220,7 +220,7 @@ class JobOfferCandidateFilter(FilterSet):
         ('recurring', _('Recurring')),
     )
     jo_type = ChoiceFilter(choices=JO_TYPE_CHOICES, method='filter_jo_type')
-    shift_dates__shift_date = DateRangeFilter(distinct=True)
+    shift__date__shift_date = DateRangeFilter(distinct=True)
 
     class Meta:
         model = hr_models.JobOffer
