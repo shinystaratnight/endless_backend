1e247498b236a904aeac768700e7777520aed2df
denys-demchenko <dendemchenko57@gmail.com>
2019-04-01 11:10:46 +0300

    RV-840 added search objects to sms

--- r3sourcer/apps/sms_interface/urls.py
+++ r3sourcer/apps/sms_interface/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url
-from .views import TemplateCompileView, ContentTypeListView, TemplateSMSMessageListView, SMSMessageListView
+from .views import TemplateCompileView, ContentTypeListView, TemplateSMSMessageListView, SMSMessageListView, SearchObjects
 
 urlpatterns = [
     url('^templates/$', TemplateSMSMessageListView.as_view(), name='templates'),
     url('^templates/compile/$', TemplateCompileView.as_view(), name='template-compile'),
     url('^find/contenttypes/$', ContentTypeListView.as_view(), name='find-contenttypes'),
+    url(r'^find/objects/(?P<ct>\d+)/$', SearchObjects.as_view(), name='find-objects'),
     url('^message/$', SMSMessageListView.as_view(), name='sms_messages')
 ]
