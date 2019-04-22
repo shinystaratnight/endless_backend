67f4b105fef7ff981a5e0b6543beba7082310a7e
denys-demchenko <dendemchenko57@gmail.com>
2019-04-22 17:39:32 +0300

    RV-791 new endpoint for tests

--- r3sourcer/apps/acceptance_tests/api/viewsets.py
+++ r3sourcer/apps/acceptance_tests/api/viewsets.py
@@ -1,4 +1,9 @@
-from r3sourcer.apps.acceptance_tests.models import AcceptanceTestRelationship
+from rest_framework.decorators import action
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+
+from r3sourcer.apps.acceptance_tests.models import AcceptanceTestRelationship, AcceptanceTest
+from r3sourcer.apps.acceptance_tests.api.serializers import AcceptanceTestSerializerAll
 from r3sourcer.apps.core.api.viewsets import BaseApiViewset
 from r3sourcer.apps.core.utils.companies import get_site_master_company
 
@@ -11,3 +16,9 @@ class AcceptanceTestViewset(BaseApiViewset):
         master_company = get_site_master_company(request=self.request)
 
         AcceptanceTestRelationship.objects.create(acceptance_test=instance, company=master_company)
+
+    @action(methods=['get'], detail=False, permission_classes=(AllowAny,))
+    def all(self, request, *args, **kwargs):
+        queryset = AcceptanceTest.objects.all()
+        serializer = AcceptanceTestSerializerAll(queryset, many=True)
+        return Response(serializer.data)
