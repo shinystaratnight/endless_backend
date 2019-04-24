3bda19123e7e4ffeb7f41801f7b83d397a0d558f
denys-demchenko <dendemchenko57@gmail.com>
2019-04-24 18:15:07 +0300

    RV-832 disable sms if balance < 0, change trial period cancel task

--- r3sourcer/apps/sms_interface/services.py
+++ r3sourcer/apps/sms_interface/services.py
@@ -317,7 +317,11 @@ class BaseSMSService(metaclass=ABCMeta):
         return from_number
 
     def substract_sms_cost(self, company, sms_message):
-        company.sms_balance.substract_sms_cost(sms_message.segments)
+        if company.sms_balance.balance > 0:
+            company.sms_balance.substract_sms_cost(sms_message.segments)
+        else:
+            raise ValueError("SMS balance should be positive, your is: {}".format(company.sms_balance.balance))
+
 
 
 class FakeSMSService(BaseSMSService):
