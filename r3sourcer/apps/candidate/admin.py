c04d78b73636230b171a426f20a1efec511b483f
denys-demchenko <dendemchenko57@gmail.com>
2019-05-03 13:31:47 +0300

    RV-793 change admin

--- r3sourcer/apps/candidate/admin.py
+++ r3sourcer/apps/candidate/admin.py
@@ -7,12 +7,17 @@ class CandidateContactAdmin(admin.ModelAdmin):
     search_fields = ('contact__first_name', 'contact__last_name')
 
 
+class CandidateRelAdmin(admin.ModelAdmin):
+    search_fields = ('master_company__name', 'candidate_contact__contact__first_name',
+                     'candidate_contact__contact__last_name')
+
+
 admin.site.register(models.VisaType)
 admin.site.register(models.SuperannuationFund)
 admin.site.register(models.CandidateContact, CandidateContactAdmin)
 admin.site.register(models.TagRel)
 admin.site.register(models.SkillRel)
 admin.site.register(models.InterviewSchedule)
-admin.site.register(models.CandidateRel)
+admin.site.register(models.CandidateRel, CandidateRelAdmin)
 admin.site.register(models.Subcontractor)
 admin.site.register(models.SubcontractorCandidateRelation)
