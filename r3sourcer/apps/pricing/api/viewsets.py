0adb6d684a4a05de57fad18347a2dcce18202bbb
kos_sur <kostyasurihin@gmail.com>
2019-06-23 23:03:34 +0300

    RV-289 fix rate coefficient modifiers

--- r3sourcer/apps/pricing/api/viewsets.py
+++ r3sourcer/apps/pricing/api/viewsets.py
@@ -66,27 +66,31 @@ class RateCoefficientViewset(core_viewsets.BaseApiViewset):
 class RateCoefficientModifierViewset(core_viewsets.BaseApiViewset):
 
     def perform_create(self, serializer):
-        default = serializer.data['default']
+        default = serializer.validated_data.get('default')
 
-        if default:
-            rate_coefficient = serializer.data['rate_coefficient']
-            rcm_type = serializer.data['type']
+        if default or default is None:
+            rate_coefficient = serializer.validated_data['rate_coefficient']
+            rcm_type = serializer.validated_data['type']
             rate_coefficient.rate_coefficient_modifiers.filter(
                 type=rcm_type, default=True
             ).update(default=False)
 
-        serializer.save()
+        instance = serializer.save()
+
+        if default is None:
+            instance.default = True
+            instance.save()
 
     def perform_update(self, serializer):
         instance = self.get_object()
-        default = serializer.data['default']
+        default = serializer.validated_data.get('default', False)
         old_default = instance.default
 
         instance = serializer.save()
 
         if default and not old_default:
-            rate_coefficient = serializer.data['rate_coefficient']
-            rcm_type = serializer.data['type']
+            rate_coefficient = serializer.validated_data['rate_coefficient']
+            rcm_type = serializer.validated_data['type']
             rate_coefficient.rate_coefficient_modifiers.filter(
                 type=rcm_type, default=True
             ).exclude(pk=instance.pk).update(default=False)
