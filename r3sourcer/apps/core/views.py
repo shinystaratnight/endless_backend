056fd0bd8857831583644a6479429a4cc2c799f9
denys-demchenko <dendemchenko57@gmail.com>
2019-07-12 13:22:41 +0300

    RV-866 added company id to roles endpoint

--- r3sourcer/apps/core/views.py
+++ r3sourcer/apps/core/views.py
@@ -149,6 +149,8 @@ class UserRolesView(APIView):
             roles = [
                 {
                     'id': x.id,
+                    '{} company_id'.format(
+                        x.company_contact_rel.company.short_name if x.company_contact_rel else x.name): x.company_contact_rel.company.id if x.company_contact_rel else x.name,
                     '__str__': '{} - {}'.format(
                         x.name, x.company_contact_rel.company.short_name or x.company_contact_rel.company.name
                     ) if x.company_contact_rel else x.name
