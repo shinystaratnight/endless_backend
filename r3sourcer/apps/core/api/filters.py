bf0f5a32bb9ad4a7113be6bc06d342fe6aaf2f25
denys-demchenko <dendemchenko57@gmail.com>
2019-07-17 14:44:01 +0300

    RV-867 change filter by status

--- r3sourcer/apps/core/api/filters.py
+++ r3sourcer/apps/core/api/filters.py
@@ -48,7 +48,8 @@ class CompanyFilter(FilterSet):
         objects = self._fetch_workflow_objects(value)
         return queryset.filter(
             Q(regular_companies__id__in=objects) |
-            Q(master_companies__id__in=objects)
+            Q(master_companies__id__in=objects) |
+            Q(master_companies__master_company__id=self.request.user.company.get_closest_master_company().id)
         ).distinct()
 
     def filter_regular_company(self, queryset, name, value):
