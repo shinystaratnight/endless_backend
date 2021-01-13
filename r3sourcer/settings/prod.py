25dfbb45aa9c1900695e47f35a50b53a6808bc35
kozlyuk <sergey.kozlyuk@gmail.com>
2021-01-13 20:54:19 +0200

    RV-1056 Allow to create a contact w/o address

--- r3sourcer/settings/prod.py
+++ r3sourcer/settings/prod.py
@@ -455,15 +455,10 @@ SMS_SEGMENT_SIZE = 160
 
 def CAN_LOGIN_AS(request, target_user): return request.user
 
-CORS_ALLOW_CREDENTIALS = True
 CORS_ORIGIN_REGEX_WHITELIST = (
     r'^(https?://)?(\w+\.)?r3sourcer(test)?\.com$',
     r'^(https?://)?r3sourcersoft(test)?\.com$',
-    r'.*amazonaws\.com$',
 )
-
-# if DEBUG:
-#     CORS_ORIGIN_REGEX_WHITELIST += (r'^(http?://)?(\w+\.)?localhost$', )
 
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
