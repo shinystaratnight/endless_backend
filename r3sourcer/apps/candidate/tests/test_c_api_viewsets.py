6f64e063bfbfe574bf1cf0cb34f1719b306ced9c
Dinara Sultangulova <oftomorrow@gmail.com>
2021-02-08 18:32:20 +0500

    RV-910 fix buy candidate contact tests

--- r3sourcer/apps/candidate/tests/test_c_api_viewsets.py
+++ r3sourcer/apps/candidate/tests/test_c_api_viewsets.py
@@ -21,6 +21,13 @@ from r3sourcer.apps.hr import models as hr_models
 from r3sourcer.apps.pricing.models import Industry
 from r3sourcer.apps.skills.models import Skill, SkillName
 
+
+def load_data():
+    # Load fixtures
+    from django.core.management import call_command
+    call_command("loaddata", "test_data")
+
+
 class CandidateContactEndpointTest(CandidateContactEndpoint):
 
     permission_classes = []
@@ -205,122 +212,84 @@ class TestBuyAPITestCase(BaseTestCase):
         if self.request_user is not None:
             return [self.request_user]
         return [core_models.User.objects.create_superuser(
-            email='test@test.mm', phone_mobile='+32345678901',
+            email='test@test.mm', phone_mobile='+79275678901',
             password='test1234'
             )]
-    def get_data(self):
-        user = core_models.User.objects.create_user(
-            email='test@test.vt', phone_mobile='+12345678801',
-            password='test1234'
-            )
-        company = core_models.Company.objects.create(
-            name='Company',
-            business_id='123',
-            type=core_models.Company.COMPANY_TYPES.master,
-            stripe_customer='cus_test'
-            )
-        test_candidate = CandidateContact.objects.create(
-            contact=user.contact, profile_price=13.0
-            )
-        company_contact = core_models.CompanyContact.objects.create(
-            contact=user.contact
-            )
-        candidate_rel = CandidateRel.objects.create(
-            candidate_contact=test_candidate,
-            master_company=company,
-            company_contact=company_contact,
-            owner=True,
-            active=True
-            )
-        return {'user': user, 'company': company, 'test_candidate': test_candidate,
-                'company_contact': company_contact, 'candidate_rel': candidate_rel}
-
 
     def setUp(self):
         super().setUp()
+        load_data()
         self.request_user = None
 
     def test_buy_success(self):
-        data = self.get_data()
-        company_2 = core_models.Company.objects.create(
-            name='Company2',
-            business_id='1233',
-            type=core_models.Company.COMPANY_TYPES.master,
-            )
-        data['candidate_rel'].master_company = company_2
-        data['candidate_rel'].save()
-        pk_ = str(data['test_candidate'].id)
-        pk_company = str(data['company'].id)
-        resp = self.make_request( method='POST', view_kwargs={"pk":pk_}, data={"company":pk_company})
-        print("RESP", resp.data)
+        resp = self.make_request(
+            method='POST',
+            view_kwargs={"pk": "63382061-88a8-4449-a84e-40c6f238ccd6"},
+            data={"company": "d882fc63-8198-4193-b5f1-935b1f6f8f8e"}
+        )
         self.assertEqual(resp.status_code, 200)
         self.assertEqual(resp.data['status'], 'success')
-        self.assertEqual(resp.data['message'], 'Please wait for payment to complete')
+        self.assertEqual(resp.data['message'], 'Please wait for candidate to agree sharing their '
+                                                           'information')
 
     def test_buy_cannot_sell_candidate(self):
-        data = self.get_data()
-        company_2 = core_models.Company.objects.create(
-            name='Company2',
-            business_id='1233',
-            type=core_models.Company.COMPANY_TYPES.master,
-            )
-        data['candidate_rel'].master_company = company_2
-        data['candidate_rel'].owner = False
-        data['candidate_rel'].save()
-        pk_ = str(data['test_candidate'].id)
-        pk_company = str(data['company'].id)
-        resp = self.make_request( method='POST', view_kwargs={"pk":pk_}, data={"company":pk_company})
+        candidate_rel = CandidateRel.objects.get(pk="d4536e9c-0d7c-4178-9943-94aafe000952")
+        candidate_rel.owner = False
+        candidate_rel.save()
+        resp = self.make_request(
+            method='POST',
+            view_kwargs={"pk": "63382061-88a8-4449-a84e-40c6f238ccd6"},
+            data={"company": "d882fc63-8198-4193-b5f1-935b1f6f8f8e"}
+        )
         self.assertEqual(resp.status_code, 400)
         self.assertEqual(resp.data['status'], 'error')
         self.assertIn('cannot sell this candidate.', resp.data['errors']['company'])
 
     def test_buy_cannot_find_company(self):
-        data = self.get_data()
-        pk_ = str(data['test_candidate'].id)
-        resp = self.make_request( method='POST', view_kwargs={"pk":pk_})
+        resp = self.make_request(
+            method='POST',
+            view_kwargs={"pk": "63382061-88a8-4449-a84e-40c6f238ccd6"},
+            data={"company": "d882fc63-8198-4193-b5f1-935b1f6f8f8d"}
+        )
         self.assertEqual(resp.status_code, 400)
         self.assertEqual(resp.data['status'], 'error')
         self.assertIn('Cannot find company',
                       resp.data['errors']['company'])
 
     def test_buy_regular_company(self):
-        data = self.get_data()
-        data['company'].type = core_models.Company.COMPANY_TYPES.regular
-        data['company'].save()
-        pk_ = str(data['test_candidate'].id)
-        pk_company = str(data['company'].id)
-        resp = self.make_request(method='POST', view_kwargs={"pk": pk_},
-                                 data={"company": pk_company})
+        company = core_models.Company.objects.get(pk="d882fc63-8198-4193-b5f1-935b1f6f8f8e")
+        company.type = core_models.Company.COMPANY_TYPES.regular
+        company.save()
+        resp = self.make_request(
+            method='POST',
+            view_kwargs={"pk": "63382061-88a8-4449-a84e-40c6f238ccd6"},
+            data={"company": "d882fc63-8198-4193-b5f1-935b1f6f8f8e"}
+        )
         self.assertEqual(resp.status_code, 400)
         self.assertEqual(resp.data['status'], 'error')
         self.assertIn("Only Master company can buy candidate's profile",
                       resp.data['errors']['company'])
 
     def test_buy_existing_rel(self):
-        data = self.get_data()
-        pk_ = str(data['test_candidate'].id)
-        pk_company = str(data['company'].id)
-        resp = self.make_request(method='POST', view_kwargs={"pk": pk_},
-                                 data={"company": pk_company})
+        resp = self.make_request(
+            method='POST',
+            view_kwargs={"pk": "63382061-88a8-4449-a84e-40c6f238ccd6"},
+            data={"company": "02d5b262-e53a-4fc0-92d6-500335910851"}
+        )
         self.assertEqual(resp.status_code, 400)
         self.assertEqual(resp.data['status'], 'error')
         self.assertIn("Company already has this Candidate Contact",
                       resp.data['errors']['company'])
 
     def test_buy_stripe_customer_not_exist(self):
-        data = self.get_data()
-        data['company'].stripe_customer = None
-        data['company'].save()
-        company_2 = core_models.Company.objects.create(
-            name='Company2',
-            business_id='1233',
-            type=core_models.Company.COMPANY_TYPES.master,
-            )
-        data['candidate_rel'].master_company = company_2
-        data['candidate_rel'].save()
-        pk_ = str(data['test_candidate'].id)
-        pk_company = str(data['company'].id)
-        resp = self.make_request(method='POST', view_kwargs={"pk":pk_}, data={"company":pk_company})
+        company = core_models.Company.objects.get(pk="d882fc63-8198-4193-b5f1-935b1f6f8f8e")
+        company.stripe_customer = None
+        company.save()
+        resp = self.make_request(
+            method='POST',
+            view_kwargs={"pk": "63382061-88a8-4449-a84e-40c6f238ccd6"},
+            data={"company": "d882fc63-8198-4193-b5f1-935b1f6f8f8e"}
+        )
         self.assertEqual(resp.status_code, 400)
         self.assertEqual(resp.data['status'], 'error')
         self.assertIn("Company has no billing information",
@@ -457,12 +426,6 @@ class TestCandidatesLocationAPITestCase(BaseTestCase):
             resp = self.make_request(data={"job_id": data['job'].id})
             location_mock.assert_called_with([data['timesheet'].id])
             self.assertEqual(resp.status_code, 200)
-
-
-def load_data():
-    # Load fixtures
-    from django.core.management import call_command
-    call_command("loaddata", "test_data")
 
 
 class TestConsentAPITestCase(BaseTestCase):
