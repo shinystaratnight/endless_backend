ea0cd6340849558d6350ca2beeab74059b6b68aa
kozlyuk <sergey.kozlyuk@gmail.com>
2021-02-09 16:12:37 +0200

    RV-1110 DATA_UPLOAD_MAX_MEMORY_SIZE increased

--- r3sourcer/settings/prod.py
+++ r3sourcer/settings/prod.py
@@ -221,6 +221,8 @@ USE_L10N = True
 
 USE_TZ = True
 
+# Data limits
+DATA_UPLOAD_MAX_MEMORY_SIZE = 5242880
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
