f9231e28346f988411040eea6801b3897a1033ed
kozlyuk <sergey.kozlyuk@gmail.com>
2021-02-11 15:48:52 +0200

    RV-1036 PriceListRate validating fix

--- r3sourcer/apps/pricing/api/serializers.py
+++ r3sourcer/apps/pricing/api/serializers.py
@@ -159,16 +159,17 @@ class PriceListRateSerializer(ApiBaseModelSerializer):
     def validate(self, data):
         skill = data.get('skill')
         uom = data.get('uom')
-        skill_rate_range = skill.skill_rate_ranges.filter(uom=uom).first()
+        worktype = data.get('worktype', None)
+        skill_rate_range = skill.skill_rate_ranges.filter(uom=uom, worktype=worktype).first()
         if skill_rate_range:
             lower_limit = skill_rate_range.price_list_lower_rate_limit
             upper_limit = skill_rate_range.price_list_upper_rate_limit
             is_lower = lower_limit and data.get('rate') < lower_limit
             is_upper = upper_limit and data.get('rate') > upper_limit
-        if is_lower or is_upper:
-            raise exceptions.ValidationError({
-                'hourly_rate': _('Hourly rate should be between {} and {}')
-                    .format(lower_limit, upper_limit)
-            })
+            if is_lower or is_upper:
+                raise exceptions.ValidationError({
+                    'hourly_rate': _('Hourly rate should be between {} and {}')
+                        .format(lower_limit, upper_limit)
+                })
 
         return data
