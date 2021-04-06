800a089cf298d4a86ff4d269c72169adda43a7ad
kozlyuk <sergey.kozlyuk@gmail.com>
2021-04-06 15:54:52 +0300

    RV-1036 add skill_rate_ranges to WorkTypeSerialize

--- r3sourcer/apps/skills/api/serializers.py
+++ r3sourcer/apps/skills/api/serializers.py
@@ -50,7 +50,7 @@ class SkillNameSerializer(ApiBaseModelSerializer):
             {'name': ('name', {'translations': ('language', 'value')})},
             {'industry': ('id', 'type', {'translations': ('language', 'value')}),
              'translations': ('language', 'value'),
-            #  'work_types': ('id', 'name', {'translations': ('language', 'value')}),
+             'work_types': ('id', 'name', {'translations': ('language', 'value')}),
             },
         )
 
@@ -118,6 +118,8 @@ class SkillRateRangeSerializer(ApiBaseModelSerializer):
 
 class WorkTypeSerializer(ApiBaseModelSerializer):
 
+    method_fields = ['skill_rate_ranges']
+
     class Meta:
         model = WorkType
         fields = (
@@ -129,3 +131,14 @@ class WorkTypeSerializer(ApiBaseModelSerializer):
             'skill_name': {'required': False},
             'skill': {'required': False}
         }
+
+    def get_skill_rate_ranges(self, obj):
+        if not obj:
+            return
+
+        if obj.skill:
+            return SkillRateRangeSerializer(obj.skill_rate_ranges.filter(skill=obj.skill),
+                                            fields=['id', 'skill', 'lower_rate_limit', 'default_rate',
+                                                    'upper_rate_limit', 'price_list_lower_rate_limit',
+                                                    'price_list_default_rate', 'price_list_upper_rate_limit'],
+                                            many=True).data
