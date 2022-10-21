8b82e141151f5ab97fe60541361254686f76b3bf
Rising Star <star-atnight@outlook.com>
2022-10-21 17:33:12 +0800

    RV-1657 - Use skill_rate property for job rate instead of skill_rate_ranges

--- r3sourcer/apps/hr/models.py
+++ r3sourcer/apps/hr/models.py
@@ -2701,10 +2701,8 @@ class TimeSheetRate(UUIDModel):
         return rate
 
     def save(self, *args, **kwargs):
-        skillraterange_default_rate = self.rate
-        self.rate = self.get_rate()
         if not self.rate or self.rate == 0:
-            self.rate = skillraterange_default_rate
+            self.rate = self.get_rate()
         self.is_hourly = self.worktype.is_hourly()
         super().save(*args, **kwargs)
 
